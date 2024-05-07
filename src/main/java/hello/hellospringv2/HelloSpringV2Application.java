8c5dceb832443da1caadb46c4b77ef674c20d85e
junjinzero <progressnull@gmail.com>
2024-05-07 18:52:11 +0900

    Initial commit

---
+++ src/main/java/hello/hellospringv2/HelloSpringV2Application.java
@@ -0,0 +1,13 @@
+package hello.hellospringv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringV2Application.class, args);
+	}
+
+}
