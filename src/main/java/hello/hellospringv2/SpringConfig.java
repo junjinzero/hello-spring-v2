de16fa0ab615095fb44a0dd40f5559188c784780
junjinzero <progressnull@gmail.com>
2024-09-01 19:17:44 +0900

    섹션 5-2 자바 코드로 직접 스프링 빈 등록하기 - setter injection 안좋은 이유 -> 호출이 쉬워져서 취약해짐 생성자 주입이 권장됨. (의존관계가 실행중에 동적으로 변하는 경우는 거의 없기 때문) - 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용한다. 정형화 되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록한다.

---
+++ src/main/java/hello/hellospringv2/SpringConfig.java
@@ -0,0 +1,24 @@
+package hello.hellospringv2;
+
+import hello.hellospringv2.repository.MemberRepository;
+import hello.hellospringv2.repository.MemoryMemberRepository;
+import hello.hellospringv2.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    // bean 을 내가 등록할거야.
+    // configuration 을 스프링이 뜰 때 읽고, 이건 스프링빈에 등록해야되는 거네? 하고 인식을 함 -> memberService 를 읽고 스프링빈에 등록해서 호출함.
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    // 이 memberRepository 를 엮어다가 위에 있는 멤버 서비스를 띄운다.
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/main/java/hello/hellospringv2/service/MemberService.java
+++ src/main/java/hello/hellospringv2/service/MemberService.java
@@ -9,17 +9,14 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
-
 
     // 회원 가입
     // rule : 동명이인 중복 불가 -> method 르 뽑음
