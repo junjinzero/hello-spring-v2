fe870269d69faa794a45b3f967496554336db3e4
junjinzero <progressnull@gmail.com>
2024-09-02 12:37:06 +0900

    섹션 6 - 2 - 회원가입 -> get방식으로 가입 페이지로 이동 -> form 안의 데이터를 post 방식으로 서버로 넘김 (submit) - name 이 서버로 넘어가는 key 가 됨 name="name" -> post 방식으로 데이터를 서버로 보냄 -> memberForm 클래스의 name 필드에 값이 들어가게 된다. (set으로 할당, get으로 출력)

--- src/main/java/hello/hellospringv2/controller/MemberController.java
+++ src/main/java/hello/hellospringv2/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospringv2.controller;
 
+import hello.hellospringv2.domain.Member;
 import hello.hellospringv2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +17,20 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
         // memberService 에 @service 어노테이션이 없으면 스프링이 서비스라고 인식하지 못함
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
