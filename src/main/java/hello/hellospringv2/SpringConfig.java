90bfbc43728788eca26cd2266cb670f7add751ab
junjinzero <progressnull@gmail.com>
2024-09-04 18:33:09 +0900

    섹션 7 - 2 - 순수 JDBC "주의! 이렇게 JDBC API로 직접 코딩하는 것은 20년 전 이야기이다. 따라서 고대 개발자들이 이렇게 고생하고 살았구나 생각하고, 정신건강을 위해 참고만 하고 넘어가자." - JdbcMemberRepository 클래스를 생성해서 MemberRepository 구현체(인터페이스)를 확장함. - 인터페이스를 두고 구현체를 확장함 memberService 가 memberRepository 를 쓰고 있는데, Repository 를 바꾸지 않아도 알아서 새로 만든 JdbcMemberRepository 를 따라간다.

--- src/main/java/hello/hellospringv2/SpringConfig.java
+++ src/main/java/hello/hellospringv2/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospringv2;
 
+import hello.hellospringv2.repository.JdbcMemberRepository;
 import hello.hellospringv2.repository.MemberRepository;
 import hello.hellospringv2.repository.MemoryMemberRepository;
 import hello.hellospringv2.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     // bean 을 내가 등록할거야.
     // configuration 을 스프링이 뜰 때 읽고, 이건 스프링빈에 등록해야되는 거네? 하고 인식을 함 -> memberService 를 읽고 스프링빈에 등록해서 호출함.
@@ -19,6 +30,7 @@ public class SpringConfig {
     // 이 memberRepository 를 엮어다가 위에 있는 멤버 서비스를 띄운다.
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
