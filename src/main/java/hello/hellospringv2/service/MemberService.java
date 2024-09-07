0b2f6dca4baea2af8bbf8b5276aac6087a0a6b39
junjinzero <progressnull@gmail.com>
2024-09-07 15:26:56 +0900

    섹션 8 - 2 - AOP 적용 - 시간 측정 로직을 한 곳에서 구성해놓고, 필요한 곳에 적용할 수 있도록 한다 -- component 로 등록해야 함 (@Component 어노테이션 달던지, SpringConfig 에 @Bean으로 등록하던지)

--- src/main/java/hello/hellospringv2/service/MemberService.java
+++ src/main/java/hello/hellospringv2/service/MemberService.java
@@ -24,18 +24,11 @@ public class MemberService {
     // rule : 동명이인 중복 불가 -> method 르 뽑음
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
+        validateDuplicateMember(member);
 
-        try {
-            validateDuplicateMember(member);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
+        memberRepository.save(member);
 
-        }
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -47,16 +40,7 @@ public class MemberService {
 
     // 전체 멤버 조회
     public List<Member> findMembers() {
-
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
-
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
